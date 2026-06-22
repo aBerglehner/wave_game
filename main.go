27b0c0bc5106dbea38d1e4294f8e1329f956e8d1
aBerglehner <alexanderberglehner2@gmail.com>
2026-06-23 00:35:23 +0200

    chore: notes

--- main.go
+++ main.go
@@ -1,5 +1,9 @@
 package main
 
+//ideas:
+//-able to jump on every lvl (so I can check bugs fast)
+//-enemies have a certain move radius and extra a certain attack range
+
 import (
 	"fmt"
 	"image"
