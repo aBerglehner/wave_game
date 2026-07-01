926c49412cde6410b215504679f846bcfe8fbce2
aBerglehner <alexanderberglehner2@gmail.com>
2026-07-01 23:39:08 +0200

    chore: notes

--- main.go
+++ main.go
@@ -175,7 +175,7 @@ func movementController(g *Game) (moveDistance float64) {
 // Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Write your game's rendering.
+	// the stuff that you draw last is on top
 	drawBackground(screen)
 
 	drawEnemies(g, screen)
